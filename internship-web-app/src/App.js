de652e8b60a7bfa05a5b2364f7ff934388f96a2e
Ondřej Svoboda <ondrej.svoboda@pslib.cz>
2022-02-17 17:42:36 +0100

    Internships login page + header

--- internship-web-app/src/App.js
+++ internship-web-app/src/App.js
@@ -1,25 +1,23 @@
+import Header from "./components/Header"
+
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
     </div>
   );
 }
+
+//Test React.Component
+
+//class App extends React.Component {
+//  render() {
+//    return <h1>React Správa Praxí</h1>;
+//  }
+//}
 
 export default App;
---
+++ internship-web-app/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+//Test function
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1 style={{ color: "#2F65B0"}}>Správa Praxí</h1>
+            <h2>Test function komponenty</h2>
+        </header>
+    )
+}
+
+export default Header
